4d9a6afd5b9682122c0e7387f90453b8e93712a7
Olivier Van Acker <cyberroadie@gmail.com>
2010-12-26 11:38:01 +0000

    Create symlinks in home directory

--- createlinks.sh
+++ createlinks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ~
-ls -A1 ~/config | grep -v .git | grep -v create | awk '{print "ln -s ./config/"$1" "$1}' | sh 
+ls -A1 ~/config | grep -v .git | grep -v create | awk '{print "ln -s ./config/"$1" ~/"$1}' | sh 
 
