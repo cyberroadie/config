194d5a46c39a780d0dce7919bc671f309d7e721a
Olivier Van Acker <cyberroadie@gmail.com>
2011-09-18 21:13:59 +0100

    Changed notification script for os x

--- bin/irssi.sh
+++ bin/irssi.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-ssh cyberroadie@bugs ": > .irssi/fnotify ; tail -f .irssi/fnotify " |
-sed -u 's/[<@&]//g' | while read heading message;
-do /usr/bin/notify-send -u critical -i /usr/share/icons/Tango-xfce/scalable/apps/terminal.svg -t 30000 -- "${heading}" "${message}";
+unamestr=`uname`
+echo "test1"
+#ssh cyberroadie.org ": > .irssi/fnotify ; echo \"irc notification enabled\" > .irssi/fnotify ; tail -f .irssi/fnotify " | sed 's/[&]//g' | while read heading message;
+ssh cyberroadie.org ": > .irssi/fnotify ; echo \"irc notification enabled\" > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading message;
+do
+  echo "test"
+  if [[ "$unamestr" == 'Linux' ]]; then
+    /usr/bin/notify-send -u critical -i /usr/share/icons/Tango-xfce/scalable/apps/terminal.svg -t 30000 -- "${heading}" "${message}";
+  elif [[ "$unamestr" == 'Darwin' ]]; then
+    /usr/local/bin/growlnotify -m "${message}" -t "${heading}" --image ~/bin/irssitop.png;
+  fi
 done
