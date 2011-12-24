7e409a73fb565770ae54f10e3df7c7dcf3582903
cyberroadie <cyberroadie@gmail.com>
2011-12-24 11:12:45 +0000

    Added Linux support to irssi notification script

--- bin/irssi.sh
+++ bin/irssi.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 unamestr=`uname`
-echo "test1"
 #ssh cyberroadie.org ": > .irssi/fnotify ; echo \"irc notification enabled\" > .irssi/fnotify ; tail -f .irssi/fnotify " | sed 's/[&]//g' | while read heading message;
 ssh cyberroadie.org ": > .irssi/fnotify ; echo \"irc notification enabled\" > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading message;
 do
-  echo "test"
-  if [[ "$unamestr" == 'Linux' ]]; then
-    /usr/bin/notify-send -u critical -i /usr/share/icons/Tango-xfce/scalable/apps/terminal.svg -t 30000 -- "${heading}" "${message}";
-  elif [[ "$unamestr" == 'Darwin' ]]; then
+  if [[ "$unamestr" == 'Linux' ]]
+  then
+    /usr/bin/notify-send -i notification-message-im -- "IRC" "Message from ${heading}";
+  elif [[ "$unamestr" == 'Darwin' ]]
+  then
     /usr/local/bin/growlnotify -m "${message}" -t "${heading}" --image ~/bin/irssitop.png;
   fi
 done
+
